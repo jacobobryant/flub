d5b33ecd7a0091beaedba1399f150b5b82059707
Jacob O'Bryant <foo@jacobobryant.com>
2019-05-29 11:53:29 -0600

    Use commit sha instead of tag in pom.xml

--- src/trident/build/pom.clj
+++ src/trident/build/pom.clj
@@ -1,16 +1,21 @@
 (ns trident.build.pom
   (:require [trident.build.xml :refer [xml-replace]]
-            [trident.build.util :refer [abspath]]
+            [trident.build.util :refer [abspath sh]]
             [clojure.data.xml :as xml]
             [clojure.tools.deps.alpha.gen.pom :as gen.pom]
             [clojure.java.io :as io]
             [clojure.tools.deps.alpha.reader :refer [read-deps]]
+            [clojure.string :refer [ends-with?]]
             [clojure.zip :as zip]))
 
 (xml/alias-uri 'pom "http://maven.apache.org/POM/4.0.0")
 
 (defn sync-pom [{:keys [group-id artifact-id version github-repo]}]
-  (let [pom-path (abspath "pom.xml")]
+  (let [clean? (= "" (sh "git" "status" "--porcelain"))
+        _ (when (not (or clean? (ends-with? version "SNAPSHOT")))
+            (throw (ex-info "Can't release without a clean commit" {})))
+        commit (sh "git" "rev-list" "-n" "1" "HEAD")
+        pom-path (abspath "pom.xml")]
     (io/delete-file pom-path true)
     (gen.pom/sync-pom
       (read-deps [(io/file (abspath "deps.edn"))])
@@ -26,7 +31,7 @@
             [::pom/scm
              [::pom/connection (str "scm:git:git://github.com/" github-repo ".git")]
              [::pom/developerConnection (str "scm:git:ssh://git@github.com/" github-repo ".git")]
-             [::pom/tag version]
+             [::pom/tag commit]
              [::pom/url (str "https://github.com/" github-repo)]]))
         zip/root
         xml/indent-str
