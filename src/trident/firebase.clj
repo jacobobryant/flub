a63c8612b62ccf564c0cf8308391802903d1273c
Jacob O'Bryant <foo@jacobobryant.com>
2019-06-28 17:06:55 -0600

    Use unique Firebase app names

--- src/trident/firebase.clj
+++ src/trident/firebase.clj
@@ -9,31 +9,39 @@
 (defn init-firebase!
   "Calls `com.google.firebase.FirebaseApp/initializeApp`.
 
-  `firebase-key`: your Firebase private key (a string)"
-  [firebase-key]
+   - `firebase-key`: your Firebase private key (a string)
+   - `app-name`: the name to initialize this app under. See
+     [[com.google.firebase.FirebaseApp/initializeApp]]"
+  [firebase-key app-name]
   (let [credentials (-> firebase-key
                         (.getBytes)
                         input-stream
                         (GoogleCredentials/fromStream))
+        _ (def credentials credentials)
         options (-> (new FirebaseOptions$Builder)
                     (.setCredentials credentials)
                     .build)]
-    (FirebaseApp/initializeApp options)))
+    (FirebaseApp/initializeApp options app-name)))
 
 (defn verify-token
   "Verifies a firebase token, returning a map of the claims.
 
-   - `token`: the user's auth token.
-   - `firebase-key`: the contents of your `*-firebase-adminsdk-*.json` file
-     (a string)
+  - `token`: the user's auth token.
+  - `firebase-key`: the contents of your `*-firebase-adminsdk-*.json` file
+  (a string)
 
-  If needed, calls [[init-firebase!]]. Returns `nil` if `token` is invalid."
+  Returns `nil` if `token` is invalid. If needed, initializes the firebase app
+  with the hash of `firebase-key` as the app name. See [[init-firebase!]]."
   [token firebase-key]
-  (when (= 0 (count (FirebaseApp/getApps)))
-    (init-firebase! firebase-key))
-  (try
-    (into {}
-          (-> (FirebaseAuth/getInstance)
-              (.verifyIdToken token)
-              (.getClaims)))
-    (catch Exception e nil)))
+  (let [app-name (str (hash firebase-key))
+        app (try
+              (FirebaseApp/getInstance app-name)
+              (catch IllegalStateException _
+                (init-firebase! firebase-key app-name)))]
+    (try
+      (into {}
+            (-> app
+                (FirebaseAuth/getInstance)
+                (.verifyIdToken token)
+                (.getClaims)))
+      (catch Exception _ nil))))
