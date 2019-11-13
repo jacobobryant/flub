559d0055dbb54fa7ed4571518cf989fb726b07bb
Jacob O'Bryant <foo@jacobobryant.com>
2019-11-12 21:15:32 -0700

    Add mount calls to trident.repl/refresh

--- src/trident/repl.clj
+++ src/trident/repl.clj
@@ -9,13 +9,10 @@
 (defmacro refresh
   "Refreshes namespaces and calls `(use 'clojure.repl)` afterwards.
 
-  `args`: passed to `clojure.tools.namespace.repl/refresh`, e.g.
-  `[:after 'mount.core/start]`.
-
-  Recommended usage: define a `refresh` macro in the `user` namespace that
-  delegates to this."
-  [& args]
-  `(do (tn/refresh ~@args)
+  Restarts mount components."
+  []
+  `(do (mount/stop)
+       (tn/refresh :after 'mount.core/start)
        (use 'clojure.repl)
        :ready))
 
@@ -25,7 +22,8 @@
   ([{:keys [nrepl-port] :or {nrepl-port 7888}}]
    (st/instrument)
    (nrepl/start-server :port nrepl-port)
-   (println "Started nrepl server on port" nrepl-port)))
+   (println "Started nrepl server on port" nrepl-port)
+   (refresh)))
 
 (defmacro defhandler [sym config]
   `(mount/defstate ~sym
